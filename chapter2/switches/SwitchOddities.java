2d4656351f08444765150c298bf7cf4d1c58e117
David Jimenez <david.jimenez@rmit.edu.au>
2016-04-26 16:39:41 +1000

    created new switch prac for oddities

---
+++ chapter2/switches/SwitchOddities.java
@@ -0,0 +1,14 @@
+public class SwitchOddities {
+	
+	public static void main(String... args) {
+		int i = 1;
+		switch(i) {
+			case 11:
+				System.out.println("11");
+			case 1:
+				System.out.println(1);
+			default:
+				System.out.println("none");
+		}
+	}
+}
