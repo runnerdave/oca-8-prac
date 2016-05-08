cb361fd4a108ad520810983c30f1916bca26c14c
runnerdave <runnerdave@gmail.com>
2016-05-08 20:06:53 +1000

    more practice with arrays

--- chapter3/MultiDimensionalArrayPrac.java
+++ chapter3/MultiDimensionalArrayPrac.java
@@ -4,6 +4,9 @@ public class MultiDimensionalArrayPrac {
       int ia[][][] = new int[i][i = 3][i];
       System.out.println( ia.length + ", " + ia[0].length+", "+ ia[0][0].length);
       int i3[][][] = new int[4][3][5];
-      System.out.println("length should be 3(the first):" + i3.length );
+      System.out.println("length should be 4(the first):" + i3.length );
+      int i4[][][] = new int[1][3][5];
+      System.out.println("length should be 1(the first):" + i4.length );
+      System.out.println(" and the second dimension should be 3:" + i4[0].length);
    }
 }
