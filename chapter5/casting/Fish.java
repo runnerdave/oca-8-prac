13c9fff8f2f632407c0909ee52c84c5dd462f956
runnerdave <runnerdave@gmail.com>
2016-05-15 07:55:15 +1000

    casting prac

--- chapter5/casting/Fish.java
+++ chapter5/casting/Fish.java
@@ -7,5 +7,39 @@ public class Fish extends Animal implements Reaction {
           Animal fishy = new Fish();
           ((Fish)fishy).unDoing();
           new Fish().unDoing();
+
+          System.out.println("--practicing ClassCastException");
+          try {
+          	((Fish)fishy).m2(); //THROWS ClassCastException
+          }
+          catch(ClassCastException cce) {
+          	System.out.println("ClassCastException caught!!");
+          	System.out.println(cce.getMessage());
+          }
+
+          System.out.println("--practicing casting");
+          Fish pureFish = new Fish();
+          pureFish.unDoing();
+          pureFish.living();
+          ((Animal)pureFish).doing();
+          System.out.println(pureFish instanceof Animal);
+
+          System.out.println("--practicing breaking the casting");
+          Animal pureAnimal = new Animal();
+          try {
+          	((Fish)pureAnimal).unDoing();
+          }
+          catch(ClassCastException cce) {
+          	System.out.println("ClassCastException caught!!");
+          	System.out.println(cce.getMessage());
+          }
      }
+
+    Object m1(){
+		return new Object();
+	}
+	
+	void m2(){
+		String s = (String) m1();
+	}
 }
