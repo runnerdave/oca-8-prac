7bde3dac4051a4dcf7505e3ab1edb647b140a64d
runnerdave <runnerdave@gmail.com>
2016-05-29 18:46:29 +1000

    mod examples and int divisions

--- chapter1/operators/Operators.java
+++ chapter1/operators/Operators.java
@@ -10,7 +10,17 @@ public class Operators{
 		}
  		return x1 + x2 + x3;
 	}
+
 	public static void main(String[] args) {
 		System.out.println(operators());
+		System.out.println("mod example 1%2:" + (1%2));
+		System.out.println("mod example 2%3:" + (2%3));
+		System.out.println("mod example 2%7:" + (2%7));
+		System.out.println("mod example 12.1%2:" + (12.1%2));
+		System.out.println("mod example 12.1/2:" + (12.1/2));
+		System.out.println("div example 1/2:" + (1/2));
+		System.out.println("div example 1/3:" + (1/3));
+		System.out.println("div example 4/3:" + (4/3));
+		System.out.println("div example 3/4:" + (3/4));
 	}
 }
