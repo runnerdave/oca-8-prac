ffc8556036df8a16914c542a7a9a360b74b10841
runnerdave <runnerdave@gmail.com>
2016-05-31 20:20:09 +1000

    double prac

--- chapter2/primaries/DoublePrac.java
+++ chapter2/primaries/DoublePrac.java
@@ -1,6 +1,18 @@
 public class DoublePrac {
 	public static void main(String... s) {
-		double dd = 012;
+		double dd = 012; 
 		System.out.println(dd);
+		/*
+		001 in octal is 1 in decimal
+		002 in octal is 2 in decimal
+		003 in octal is 3 in decimal
+		004 in octal is 4 in decimal
+		005 in octal is 5 in decimal
+		006 in octal is 6 in decimal
+		007 in octal is 7 in decimal
+		010 in octal is 8 in decimal
+		011 in octal is 9 in decimal
+		012 in octal is 10 in decimal
+		*/
 	}
 }
