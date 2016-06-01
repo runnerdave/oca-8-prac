3857ac6458352060f60ae53387225b41c30a1098
David Jimenez <david.jimenez@rmit.edu.au>
2016-06-01 16:10:03 +1000

    check rmit box

--- chapter1/ThisPrac.java
+++ chapter1/ThisPrac.java
@@ -5,7 +5,7 @@ public class ThisPrac {
 	int j;
 
 	public static void main(String... a){
-		//System.out.println("Prac for this i:" + this.i); DOES NOT COMPILE
+		//System.out.println("Prac  for this i:" + this.i); DOES NOT COMPILE
 		//System.out.println("Prac for this j:" + this.j); DOES NOT COMPILE
 		ThisPrac tp = new ThisPrac();		
 		tp.printerI();
