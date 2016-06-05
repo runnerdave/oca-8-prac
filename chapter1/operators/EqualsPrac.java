45859e5804c6e3a0233775ff75e908d0d31ab1f8
runnerdave <runnerdave@gmail.com>
2016-06-06 06:59:03 +1000

    equals pracs

---
+++ chapter1/operators/EqualsPrac.java
@@ -0,0 +1,38 @@
+public class EqualsPrac{
+	
+	public static void main(String... a) {
+		System.out.println("===========Practice == with primitives ==== ");
+		int i = 2;
+		short s = 2;
+		double d = 2.0;
+		float f = 2.0f;
+
+		if (i==s) {
+			System.out.println("short and int contain 2 and are equal.");
+		} else {
+			System.out.println("short and int contain 2 and are not equal.");
+		}
+		s=3;
+		if (i==s) {
+			System.out.println("short contains 3 and int contain 2 and are equal.");
+		} else {
+			System.out.println("short contains 3 and int contain 2 and are not equal.");
+		}
+		if (i==d) {
+			System.out.println("double and int contain 2 and are equal.");
+		} else {
+			System.out.println("double and int contain 2 and are not equal.");
+		}
+		if (d==i) {
+			System.out.println("double and int contain 2 and are equal.");
+		} else {
+			System.out.println("double and int contain 2 and are not equal.");
+		}
+		if (f==i) {
+			System.out.println("float and int contain 2 and are equal.");
+		} else {
+			System.out.println("float and int contain 2 and are not equal.");
+		}
+
+	}
+}
--- chapter2/primaries/WrappersPrac.java
+++ chapter2/primaries/WrappersPrac.java
@@ -3,10 +3,16 @@ public class WrappersPrac {
 		int i = 1;
 		Integer ii = new Integer(1);
 		Integer ij = new Integer(1);
+		Integer iii = 1;
+		Short ss = 1;
+		short s = 1;
 
 		System.out.println("test if i and ii are equals with ==:" +(i==ii));
 		System.out.println("test if i and ii are equals with equals:" +(ii.equals(i)));
 		System.out.println("test if ij and ii are equals with equals:" +(ii.equals(ij)));
 		System.out.println("test if ij and ii are equals with ==:" +(ij==ii));
+		System.out.println("test if ij and iii are equals with equals:" +(iii.equals(ij)));
+		System.out.println("test if ss and iii are equals with equals:" +(iii.equals(ss)));
+		System.out.println("test if s and i are equals with ==:" +(i==s));
 	}
 }
