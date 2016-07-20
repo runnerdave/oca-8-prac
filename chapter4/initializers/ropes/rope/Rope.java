76934bef1141d3a9ba3adff387bd1cf8d942af87
runnerdave <runnerdave@gmail.com>
2016-07-21 07:30:45 +1000

    adding new case of static initializers

---
+++ chapter4/initializers/ropes/rope/Rope.java
@@ -0,0 +1,11 @@
+package rope;
+public class Rope {
+	{
+		System.out.println("in instance constructor");
+	}
+	static {
+		System.out.println("in static constructor");
+	}
+	public static int length = 0;
+	
+}
