f193edf039c2b680344f1dbe239adf9ca2cc11c9
runnerdave <runnerdave@gmail.com>
2016-08-10 20:43:02 +1000

    odd cases for nested classes

--- ocp/chapter1/nestedClasses/Outer.java
+++ ocp/chapter1/nestedClasses/Outer.java
@@ -17,5 +17,13 @@ public class Outer {
 		outer.callInner();
 		Inner inner = outer.new Inner();
 		inner.sayHello();
+
+		Outer.Inner in = new Outer().new Inner();
+
+		//Inner in = new Inner(); //DOES NOT COMPILE BECAUSE IS NOT STATIC
+		//Inner in = Outer.new Inner(); //DOES NOT FIND SYMBOL FOR VARIABLE OUTER
+		//Outer.Inner in = new Outer.Inner(); //DOES NOT COMPILE BECAUSE IS NOT STATIC
+		//Outer.Inner in = new Outer().Inner(); //DOES NOT COMPILE BECAUSE Inner() is seen as a method
+		//Outer.Inner in = Outer.new Inner(); //DOES NOT FIND SYMBOL FOR VARIABLE OUTER
 	}
 }
