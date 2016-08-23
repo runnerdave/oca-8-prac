a5e271b7460f61b8ded96b27ce889af0c9c882ac
runnerdave <runnerdave@gmail.com>
2016-08-24 06:48:48 +1000

    bound and list pracs

--- ocp/chapter3/collections/ArrayListPrac.java
+++ ocp/chapter3/collections/ArrayListPrac.java
@@ -4,7 +4,7 @@ public class ArrayListPrac {
 		List myList = new ArrayList();
 		myList.add("hola");
 		myList.add("bola");
-		myList.add(4);
+		myList.add("dd");
 		System.out.println(myList.toString());
 		for(Object s : myList) {
 			System.out.println(s);
