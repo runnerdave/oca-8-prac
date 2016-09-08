17ea1e7eebe662d957dc1b26d64a6c59e4a88633
David Jimenez <david.jimenez@rmit.edu.au>
2016-09-08 11:39:44 +1000

    java resource bundles

--- ocp/chapter5/l18n/ZooOpen.java
+++ ocp/chapter5/l18n/ZooOpen.java
@@ -6,10 +6,27 @@ public class ZooOpen {
 		printProperties(us); 
 		System.out.println(); 
 		printProperties(france);
+
+		System.out.println("====using Properties object===");
+		usePropertiesObject(france);
 	}
 	public static void printProperties(Locale locale) { 
 		ResourceBundle rb = ResourceBundle.getBundle("Zoo", locale); 
 		System.out.println(rb.getString("hello")); 
 		System.out.println(rb.getString("open"));
+		//System.out.println(rb.getString("hola")); THROWS EXCEPTION
 	} 
+
+	public static void usePropertiesObject(Locale locale) {
+		Properties props = new Properties();
+
+		ResourceBundle rb = ResourceBundle.getBundle("Zoo", locale); 
+		rb.keySet().stream()
+			.forEach(k -> props.put(k, rb.getString(k)));
+
+		System.out.println("with getProperty:" + props.getProperty("hello"));
+		System.out.println("with get:" + props.get("hello"));
+		System.out.println("with getProperty for property that does not exist:" + props.getProperty("hola")); //retuns null
+		System.out.println("with getProperty for property that does not exist with default:" + props.getProperty("hola", "defaulte")); //retuns defaulte
+	}
 }
