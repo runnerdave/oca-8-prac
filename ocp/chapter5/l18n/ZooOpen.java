c8f28b6f7f8534de16d539a28358b470b073625f
runnerdave <runnerdave@gmail.com>
2016-09-09 06:46:24 +1000

    java resourcebundles

--- ocp/chapter5/l18n/ZooOpen.java
+++ ocp/chapter5/l18n/ZooOpen.java
@@ -3,9 +3,15 @@ public class ZooOpen {
 	public static void main(String[] args) {
 	 	Locale us = new Locale("en", "US");
 	 	Locale france = new Locale("fr", "FR");
+	 	Locale venezuela = new Locale("es", "VE");
+	 	Locale spain = new Locale("es", "ES");
 		printProperties(us); 
 		System.out.println(); 
 		printProperties(france);
+		System.out.println(); 
+		printProperties(venezuela);
+		System.out.println(); 
+		printProperties(spain);
 	}
 	public static void printProperties(Locale locale) { 
 		ResourceBundle rb = ResourceBundle.getBundle("Zoo", locale); 
