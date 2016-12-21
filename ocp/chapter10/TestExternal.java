f0763ca9917d03f97fdf10bca9951c2e7b2c7474
runnerdave <runnerdave@gmail.com>
2016-12-22 07:17:34 +1100

    adding mysql connector

--- ocp/chapter10/TestExternal.java
+++ ocp/chapter10/TestExternal.java
@@ -2,7 +2,7 @@ import java.sql.*;
 public class TestExternal {
 	public static void main(String[] args) throws SQLException {
 		Connection conn = DriverManager.getConnection(
-			"jdbc:mysql://localhost:3306/springtutorial",
+			"jdbc:mysql://localhost:3306/springtutorial?autoReconnect=true&useSSL=false",
 			"udemy",
 			"letmein");
 		System.out.println(conn);
--- ocp/chapter10/readme.md
+++ ocp/chapter10/readme.md
@@ -4,3 +4,5 @@ java -cp "$JAVA_HOME/db/lib/derby.jar:." SetupDerbyDatabase
 mysql:
 
 ~/.m2/repository/mysql/mysql-connector-java/5.1.37/mysql-connector-java-5.1.37.jar
+java -cp "lib/mysql-connector-java-5.1.40-bin.jar:." TestExternal
+
