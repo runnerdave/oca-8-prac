6633134629b134828cc071deb038bce93de6280a
David Jimenez <david.jimenez@rmit.edu.au>
2016-12-22 16:51:39 +1100

    scrolling example

--- ocp/chapter10/TestExternal.java
+++ ocp/chapter10/TestExternal.java
@@ -7,12 +7,14 @@ public class TestExternal {
 			"jdbc:mysql://localhost:3306/springtutorial?autoReconnect=true&useSSL=false",
 			"udemy",
 			"letmein");
+
+        System.out.println("== Print the connection object ==");
 		System.out.println(conn);
 
         Statement stmt = conn.createStatement( ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
 
         ResultSet rs = stmt.executeQuery("select * from messages");
-
+        System.out.println("== Print the ResultSet object ==");
         System.out.println(rs);
 
         Map<Integer, String> idToNameMap = new HashMap<>();
@@ -21,5 +23,23 @@ public class TestExternal {
             String name = rs.getString("name");
             idToNameMap.put(id, name);
         }
+        System.out.println("== Print results from messages forward only ==");
         System.out.println(idToNameMap); 
+
+        System.out.println();
+        System.out.println("== Print results from messages with scrolling ==");
+        stmt = conn.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        rs = stmt.executeQuery("select * from messages");
+        rs.afterLast();
+        System.out.println(rs.previous()); // true
+        System.out.println(rs.getInt(1)); // 2        
+        System.out.println(rs.previous()); // true
+        System.out.println(rs.getInt(1)); // 1
+        System.out.println(rs.last()); // true
+        System.out.println(rs.getInt(1)); // 2
+        System.out.println(rs.first()); // true
+        System.out.println(rs.getInt(1)); // 1
+        rs.beforeFirst();
+        System.out.println(rs.getInt(1)); // throws SQLException
+    }
 }
