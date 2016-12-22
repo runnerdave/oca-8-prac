1e57e3a631871c311afb3e6153dc561237a62110
David Jimenez <david.jimenez@rmit.edu.au>
2016-12-22 16:22:10 +1100

    read data from springtutorial

--- ocp/chapter10/TestExternal.java
+++ ocp/chapter10/TestExternal.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.util.*;
+
 public class TestExternal {
 	public static void main(String[] args) throws SQLException {
 		Connection conn = DriverManager.getConnection(
@@ -6,5 +8,18 @@ public class TestExternal {
 			"udemy",
 			"letmein");
 		System.out.println(conn);
-	}
+
+        Statement stmt = conn.createStatement( ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+
+        ResultSet rs = stmt.executeQuery("select * from messages");
+
+        System.out.println(rs);
+
+        Map<Integer, String> idToNameMap = new HashMap<>();
+        while(rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            idToNameMap.put(id, name);
+        }
+        System.out.println(idToNameMap); 
 }
