89a8df23783b5fefa12ec4d5668c919ea80431cb
runnerdave <runnerdave@gmail.com>
2017-03-08 07:20:27 +1100

    set example

--- ocp/chapter3/collections/NavigableSetPrac.java
+++ ocp/chapter3/collections/NavigableSetPrac.java
@@ -7,5 +7,18 @@ public class NavigableSetPrac {
 		System.out.println(set.floor(10)); // 10
 		System.out.println(set.ceiling(20)); // 20
 		System.out.println(set.higher(20)); // null
+
+		System.out.println("===with strings===");
+		NavigableSet<String> stringSet = new TreeSet<>();
+		stringSet.add("ee");
+		stringSet.add("a");
+		stringSet.add("b");
+		stringSet.add("aa");
+		stringSet.add("d");
+
+		for(String s: stringSet) {
+			System.out.println(s);
+		}
+
 	}
 }
